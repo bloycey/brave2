e073dd831637ecdf03bdea905cc4ad3c66448c22
Andrew Bloyce <bloyce1990@gmail.com>
2020-08-11 08:52:43 +1000

    Added min-width to qty field

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,28 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var path = require('path');
-var fs = require('fs');
+var gulp = require("gulp");
+var less = require("gulp-less");
+var path = require("path");
+var fs = require("fs");
 
-gulp.task('less', function (done) {
-	fs.stat('./src/css/less/app.less', function(err, stat) {
-		if(err != null) {
-			console.log('Error:' + err.code);
-		}
-	});
-	gulp.src('./src/css/less/app.less')
-	.pipe(less({
-		paths: [ path.join(__dirname, 'less', 'includes') ]
-	}))
-	.pipe(gulp.dest('./src/css/'));
-	done();
+gulp.task("less", function (done) {
+    fs.stat("./src/css/less/app.less", function (err, stat) {
+        if (err != null) {
+            console.log("Error:" + err.code);
+        }
+    });
+    gulp.src("./src/css/less/app.less")
+        .pipe(
+            less({
+                paths: [path.join(__dirname, "less", "includes")]
+            })
+        )
+        .pipe(gulp.dest("./src/css/"));
+    done();
 });
 
-gulp.task('default', function (done) {
-	gulp.watch(['./src/css/less/*.less', './src/css/less/_*.less'], ['less']);
-	done();
+gulp.task("default", function (done) {
+    gulp.watch(
+        ["./src/css/less/*.less", "./src/css/less/_*.less"],
+        gulp.series(["less"])
+    );
+    done();
 });
